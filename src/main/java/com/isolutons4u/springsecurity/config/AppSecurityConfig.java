c4cece80102f004692f426f93477b1eb46b49ae4
Sushil Dangi <dangi.sushil5@gmail.com>
2018-03-12 22:35:15 +0530

    plain-login.jsp added

--- src/main/java/com/isolutons4u/springsecurity/config/AppSecurityConfig.java
+++ src/main/java/com/isolutons4u/springsecurity/config/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package com.isolutons4u.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -19,6 +20,18 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 		.withUser(users.username("sushil").password("12345").roles("EMPLOYEE"))
 		.withUser(users.username("dangi").password("12345").roles("MANAGER"))
 		.withUser(users.username("skd").password("12345").roles("ADMIN"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/showMyLoginPage")
+		.loginProcessingUrl("/authenticateTheUser")
+		.permitAll();
 	}
 
 	
---
+++ src/main/java/com/isolutons4u/springsecurity/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.isolutons4u.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/showLoginPage")
+	public String showLoginPage() {
+		
+		return "plain-login";
+	}
+
+}
