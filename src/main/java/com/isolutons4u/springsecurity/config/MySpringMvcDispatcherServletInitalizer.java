5c0e4fc404fc1faac1844db3b5e1c03787dd0e07
Sushil Dangi <dangi.sushil5@gmail.com>
2018-03-12 21:00:33 +0530

    Project Setup and test

---
+++ src/main/java/com/isolutons4u/springsecurity/config/MySpringMvcDispatcherServletInitalizer.java
@@ -0,0 +1,25 @@
+package com.isolutons4u.springsecurity.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitalizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] { "/" };
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>isolutions4u Company Home Page</title>
+</head>
+<body>
+<h1>isolutions4u Company Home Page</h1>
+<hr>
+Welcome to the isolutions4u company Home Page
+</body>
+</html>
