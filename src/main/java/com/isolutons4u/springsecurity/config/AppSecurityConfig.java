eda5aa2bd0b4be4eeaf9007c1509af2133c6b75f
Sushil Dangi <dangi.sushil5@gmail.com>
2018-03-12 22:13:44 +0530

    Basic Security

---
+++ src/main/java/com/isolutons4u/springsecurity/config/AppSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.isolutons4u.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(users.username("sushil").password("12345").roles("EMPLOYEE"));
+	}
+
+	
+}
---
+++ src/main/java/com/isolutons4u/springsecurity/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.isolutons4u.springsecurity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
